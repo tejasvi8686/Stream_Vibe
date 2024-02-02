27279caa8a538ced60e6b86fddb7f8362f7d0f4f
tejasvi raj <tejasviraj8686@gmail.com>
2024-02-02 19:10:46 +0530

    api testing sucess

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,37 @@
 import "./App.scss";
 import { useState, useEffect } from "react";
 import { fetchDataFromApi } from "./utils/api";
+
 function App() {
+  const [movies, setMovies] = useState([]);
+
   useEffect(() => {
     apiTesting();
   }, []);
+
   const apiTesting = () => {
     fetchDataFromApi("/movie/popular").then((res) => {
-      console.log(res);
+      // Assuming the API response contains an array of movies
+      setMovies(res.results);
     });
   };
+
   return (
     <div className="text">
-      Lorem, ipsum dolor sit amet consectetur adipisicing elit. Exercitationem
-      nihil id, et quidem provident ratione expedita! Similique vel aliquam
-      quidem?
+      <h1 style={{textAlign: "center"}}>Popular Movies</h1>
+      <ul style={{display : "flex", flexWrap: "wrap", listStyle: "none", gap: "10px", justifyContent: "center"}}>
+        {movies.map((movie) => (
+          <li key={movie.id}>
+            <img
+              height="450px"
+              width="450px"
+              src={`https://image.tmdb.org/t/p/w500/${movie.poster_path}`}
+              alt={movie.title}
+            />
+            <p style={{fontSize: "22px", fontWeight: "bold" }}>{movie.title}</p>
+          </li>
+        ))}
+      </ul>
     </div>
   );
 }
